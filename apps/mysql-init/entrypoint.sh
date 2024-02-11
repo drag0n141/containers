0c90a334e630837e756247adaf1c66a248df93f2
DrAg0n141 <44865095+drag0n141@users.noreply.github.com>
2024-02-11 23:24:10 +0100

    Update entrypoint.sh

--- apps/mysql-init/entrypoint.sh
+++ apps/mysql-init/entrypoint.sh
@@ -23,26 +23,26 @@ export USER="${INIT_MYSQL_SUPER_USER}"
 export MYSQL_PWD="${INIT_MYSQL_SUPER_PASS}"
 
 user_exists=$(\
-    mariadb -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '${INIT_MYSQL_USER}');"
+    mysql -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '${INIT_MYSQL_USER}');"
 )
 
 if [[ -z "${user_exists}" ]]; then
     printf "\e[1;32m%-6s\e[m\n" "Create User ${INIT_MYSQL_USER} ..."
-    mariadb -e "CREATE USER IF NOT EXISTS '${INIT_MYSQL_USER}'@'%' IDENTIFIED BY '${INIT_MYSQL_PASS}';"
+    mysql -e "CREATE USER IF NOT EXISTS '${INIT_MYSQL_USER}'@'%' IDENTIFIED BY '${INIT_MYSQL_PASS}';"
 else
     printf "\e[1;32m%-6s\e[m\n" "Update password for user ${INIT_MYSQL_USER} ..."
-    mariadb -e "ALTER USER '${INIT_MYSQL_USER}'@'%' IDENTIFIED BY '${INIT_MYSQL_PASS}';"
+    mysql -e "ALTER USER '${INIT_MYSQL_USER}'@'%' IDENTIFIED BY '${INIT_MYSQL_PASS}';"
 fi
 
 for dbname in ${INIT_MYSQL_DBNAME}; do
     database_exists=$(\
-        mariadb -e \
+        mysql -e \
             "SHOW DATABASES LIKE '${dbname}';"
     )
     if [[ -z "${database_exists}" ]]; then
         printf "\e[1;32m%-6s\e[m\n" "Create Database ${dbname} ..."
-        mariadb -e "CREATE DATABASE IF NOT EXISTS ${dbname};"
+        mysql -e "CREATE DATABASE IF NOT EXISTS ${dbname};"
     fi
     printf "\e[1;32m%-6s\e[m\n" "Update User Privileges on Database ${dbname} ..."
-    mariadb -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${INIT_MYSQL_USER}'@'%'; FLUSH PRIVILEGES;"
+    mysql -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${INIT_MYSQL_USER}'@'%'; FLUSH PRIVILEGES;"
 done
