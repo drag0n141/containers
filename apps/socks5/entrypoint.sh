b042e82374ffebc6fb7f92b5eaaf65a993ff3038
DrAg0n141 <44865095+drag0n141@users.noreply.github.com>
2024-02-13 20:22:39 +0100

    Change Config

--- apps/socks5/entrypoint.sh
+++ apps/socks5/entrypoint.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 
-#shellcheck disable=SC1091
-test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
-
-if [[ ! -f "/config/sockd.conf" ]]; then
-    printf "Copying over default configuration ...\n"
-    cp /app/sockd.conf /config/sockd.conf
-fi
-
 #shellcheck disable=SC2086
 exec \
     /usr/sbin/sockd \
-        -f /config/sockd.conf \
+        -f /app/sockd.conf \
         "$@"
