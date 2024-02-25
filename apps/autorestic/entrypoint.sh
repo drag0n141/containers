c41a606be66376c3328a16828f81d4316666f2d9
Sven Jaeger <social.github@d141.de>
2024-02-25 12:04:32 +0100

    Add Apprise

--- apps/autorestic/entrypoint.sh
+++ apps/autorestic/entrypoint.sh
@@ -3,8 +3,10 @@
 #shellcheck disable=SC2086
 exec \
     /usr/local/bin/autorestic -c "${AUTORESTIC_CONFIG}" \
-        cron "$@"
+        check "$@"
 
-exec \
-    /usr/sbin/crond -f -S -l 0 \
-        -c /etc/crontabs
+while true; do
+   autorestic -c "${AUTORESTIC_CONFIG}" --ci cron
+   #### SLEEP 5 MIN ####
+   sleep 300
+done
