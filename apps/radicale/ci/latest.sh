0894c40b96da5c8a9191b5053adffa9442d556eb
DrAg0n141 <44865095+drag0n141@users.noreply.github.com>
2024-08-23 08:28:23 +0200

    Update latest.sh

--- apps/radicale/ci/latest.sh
+++ apps/radicale/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 version="$(curl -sX GET "https://api.github.com/repos/Kozea/Radicale/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
 version="${version#*v}"
-version="${version#*release-}"
+version="${version%%-*}"
 printf "%s" "${version}"
