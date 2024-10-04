2a5f038e8416c02c611ff1c0bebbfc51ae85cb61
DrAg0n141 <44865095+drag0n141@users.noreply.github.com>
2024-10-04 19:36:24 +0200

    Update latest.sh

--- apps/socks5/ci/latest.sh
+++ apps/socks5/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://pkgs.alpinelinux.org/package/v3.20/main/x86_64/dante-server" | grep -oP '(?<=<strong>).*?(?=</strong>)' 2>/dev/null)
+version=$(curl -sX GET "https://pkgs.alpinelinux.org/package/v3.20/community/x86_64/dante-server" | grep -oP '(?<=<strong>).*?(?=</strong>)' 2>/dev/null)
 version="${version%%_*}"
 version="${version%%-*}"
 printf "%s" "${version}"
